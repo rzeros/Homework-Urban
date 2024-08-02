1b2a7358b805ba69b490f39e88fe818153cf0496
Ruslan <rslanzdg@gmail.com>
2024-08-02 11:34:34 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+# Практическое задание по теме "Переменные"
+homework_completed = 12
+course_name = 'Python'
+hours_spent = 1.5
+time_per_homework = hours_spent / homework_completed
+print('Курс:', course_name + ', всего задач:' + str(homework_completed) + ',', 'затрачено часов:', str(hours_spent) + ',', 'среднее время выполнения', time_per_homework, 'часа.')
+# Преобразовал типы данных из числовых(int и float) в строковой(str) у переменных homework_completed и hours_spent для того, чтобы провести операцию конкатенации строк.
