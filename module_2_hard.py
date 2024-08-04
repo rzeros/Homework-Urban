8073d862d7832a15e0cd8d1ac9a1c7f08e240ba4
Ruslan <rslanzdg@gmail.com>
2024-08-04 14:48:21 +0500

    Update module_2_hard.py
    Подправил код под стандарты PEP8

--- module_2_hard.py
+++ module_2_hard.py
@@ -2,9 +2,9 @@
 number = int(input('Введите число из первого поля (от 3 до 20): '))
 code = ''
 for i in range(1, 20):
-     for n in range (1, 20):
-         if number % (i + n) != 0:
-             continue
-         elif n > i:
-             code += str(i) + str(n)
+    for n in range(1, 20):
+        if number % (i + n) != 0:
+            continue
+        elif n > i:
+            code += str(i) + str(n)
 print('Число для второго поля:', code)
