dce24a56bebbea9472d18efd5d76462bd3b213ee
Ruslan <rslanzdg@gmail.com>
2024-08-10 22:00:05 +0500

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,16 @@
+# Домашняя работа по уроку "Способы вызова функции"
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if not all('@' in i and i.endswith(('.com', '.ru', '.net')) for i in (recipient, sender)):
+        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == "university.help@gmail.com":
+        print('Письмо успешно отправлено с адреса <sender> на адрес <recipient>.')
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.')
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
