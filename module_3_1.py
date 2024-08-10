32e62b13eb41aa4814be88dc4e9c53d697e1e321
Ruslan <rslanzdg@gmail.com>
2024-08-10 19:52:56 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+# Домашняя работа по уроку "Пространство имён"
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in (x.lower() for x in list_to_search)
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
