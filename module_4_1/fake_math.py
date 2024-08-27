111b48a7cf6db0b361f87d157bc9617bc446b7a2
Ruslan <rslanzdg@gmail.com>
2024-08-27 18:02:11 +0500

    Add files via upload

---
+++ module_4_1/fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        result = first / second
+        return result
+
---
+++ module_4_1/module_4_1.py
@@ -0,0 +1,7 @@
+from module_4_1.fake_math import divide as fake_divide
+from module_4_1.true_math import divide as true_divide
+
+res1 = fake_divide(5, 0)
+res2 = true_divide(9, 0)
+print(res1)
+print(res2)
