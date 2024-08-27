c4e4e36b93a69f2e4c05a2c81e9118de4bd9fd7b
Ruslan <rslanzdg@gmail.com>
2024-08-27 18:03:23 +0500

    Update module_4_1.py

--- module_4_1/module_4_1.py
+++ module_4_1/module_4_1.py
@@ -1,5 +1,5 @@
-from module_4_1.fake_math import divide as fake_divide
-from module_4_1.true_math import divide as true_divide
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
 
 res1 = fake_divide(5, 0)
 res2 = true_divide(9, 0)
